cc90f3cb606569ec4e256c5be516f79f004da62f
piekstra <piekstra@users.noreply.github.com>
2021-06-23 17:49:38 -0700

    v1.0.1 Fix linting issues (#1)

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,3 +1,17 @@
-from aws_cognito import AwsCognitoApi
+import pytest
+# from aws_cognito import AwsCognitoApi
+
 
 # TODO - This needs tests!
+@pytest.fixture(scope='module')
+def client():
+    return None
+    # return AwsCognitoApi(access_key, secret_key, region_name)
+
+
+@pytest.mark.usefixtures('client')
+class TestClient(object):
+
+    def test(self, client):
+        # TODO!
+        assert True
